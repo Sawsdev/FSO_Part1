43a01c898c2078b13d8e33dc8068d3df738cb3eb
Sergio Salazar Williams <sawsdevx8@gmail.com>
2024-01-23 10:09:55 -0500

    Add unicafe step1

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,14 +1,44 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Header = ({text}) => <h1>{text}</h1>
+const Statistic = ({name, amount}) => <p>{name}: {amount}</p>
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const addGoodFeedback = () => {
+    const updatedGood = good +1
+    setGood(updatedGood)
+  }
+
+
+  const addBadFeedback = () => {
+    const updatedBad = bad +1
+    setBad(updatedBad)
+  }
+
+  const addNeutralFeedback = () => {
+    const updatedNeutral = neutral +1
+    setNeutral(updatedNeutral)
+  }
+
+
   return (
     <div>
-      code here
+      <Header text="give feedback" />
+      <Button handleClick={addGoodFeedback} text="good" />
+      <Button handleClick={addNeutralFeedback} text="neutral" />
+      <Button handleClick={addBadFeedback} text="bad" />
+      <br />
+      <Header text="statistics" />
+      <Statistic name="good" amount={good} />
+      <Statistic name="neutral" amount={neutral} />
+      <Statistic name="bad" amount={bad} />
     </div>
   )
 }
